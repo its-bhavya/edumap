be66240ef092eb7e2f68678f031237f1b14af06d
its-bhavya <bhavyaa716@gmail.com>
2025-06-16 12:36:36 +0530

    modified extractor logic

--- backend/app/agents/extractor_agent.py
+++ backend/app/agents/extractor_agent.py
@@ -2,13 +2,16 @@ import dspy
 
 
 class StructureExtractionSignature(dspy.Signature):
+
     transcript = dspy.InputField(desc="The full transcript of a lecture or explanation.")
     central_topic = dspy.OutputField(desc="The central concept of the transcript.")
     subtopics = dspy.OutputField(
         desc="""A raw JSON list where each subtopic has:
         - 'title': string
-        - 'description': string
-        - optional 'children': a list of similar objects"""
+        - 'description': a brief phrase 
+        - optional 'children': a list of similar subtopic objects
+        If more explanation is needed, use nested subtopics under 'children' instead of a long description.
+        Subtopics can have their own subtopics up till it is relevant."""
     )
 
 class MindmapExtractor(dspy.Module):
